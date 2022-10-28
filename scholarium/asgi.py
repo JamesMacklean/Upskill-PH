bfde9bee490598141b462c4cce2591296bbae7dc
Ubuntu <ubuntu@ip-172-31-47-84.ap-northeast-1.compute.internal>
2022-10-28 12:32:41 +0000

    renamed scholariumtest to scholarium for dir ease

--- scholarium/asgi.py
+++ scholarium/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scholariumtest.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scholarium.settings')
 
 application = get_asgi_application()
--- scholarium/wsgi.py
+++ scholarium/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scholariumtest.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scholarium.settings')
 
 application = get_wsgi_application()
