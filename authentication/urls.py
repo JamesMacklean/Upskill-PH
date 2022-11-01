4723928a65c71c9ac0c1b456d957dbec6faa9858
mark <markallensongco@gmail.com>
2022-11-01 17:31:51 +0800

    login app renamed to authentication + features

---
+++ authentication/urls.py
@@ -0,0 +1,15 @@
+from unicodedata import name
+from django.contrib import admin
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+    path('',views.home, name="home"),
+    path('signup',views.signup, name="signup"),
+    path('signin',views.signin, name="signin"),
+    path('signout',views.signout, name="signout"),
+    path('success', views.success, name="success"),
+    path('activate/<uidb64>/<token>', views.activate, name="activate"),
+    path('profile', views.profile, name="profile"),
+    path('pfofile/edit', views.edit_profile, name="edit profile")
+]
---
+++ authentication/views.py
@@ -0,0 +1,109 @@
+from base64 import urlsafe_b64decode
+from email.message import EmailMessage
+from urllib import request
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from scholarium import settings
+from django.core.mail import send_mail
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes, force_text
+from . tokens import generate_token
+
+
+import authentication
+# Create your views here.
+
+def home(request):
+    return render(request,"index.html")
+
+def success(request):
+    return render(request,"welcome.html")
+
+def signup(request):
+
+    if request.method == "POST":
+        username = request.POST['username']
+        email= request.POST['email']
+        firstname= request.POST['firstname']
+        lastname= request.POST['lastname']
+
+        if User.objects.filter(username=username):
+            messages.error(request, "username already exists")
+        
+        if User.obects.filter(email=email):
+            messages.error(request, "email already exists")
+
+        
+        myuser = User.objects.create_user(username, email, firstname, lastname)
+        myuser.is_active = False
+        myuser.save()
+
+        current_site = get_current_site(request)
+        subject = "Scholarium: Account Verification"
+        message = render_to_string('email_confirmation.html',{
+            'name': myuser.firstname,
+            'domain': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
+            'token': generate_token.make_token(myuser)
+        })
+        email = EmailMessage(
+            subject,
+            message
+            settings.EMAIL_HOST_USER,
+            [myuser.email],
+        )
+        email.fail_silently = True
+        email.send()
+
+        return redirect('success')
+
+    return render(request, "authentication/signup.html")
+
+def signin(request):
+
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request,user)
+            firstname = user.firstname
+            return render(request, "authentication/dashboard.html", {'first_name': firstname})
+
+        else: 
+            messages.error(request, "Invalid username/password")
+            return redirect('home')
+
+    return render(request, "authentication/signin.html")
+
+def signout(request):
+    logout(request)
+    messages.success(request, "Logged out successfully")
+    return redirect('home')
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_b64decode(uidb64))
+        myuser = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        myuser= None
+    
+    if myuser is not None and generate_token.check_token(myuser, token):
+        myuser.is_active = True
+        myuser.save()
+        login(request, myuser)
+        return redirect('signin')
+    else:
+        return(request, "activation_failed.html")
+
+def profile(request):
+    return render(request, "profile.html")
+
+def edit_profile(request):
+    return(request, "edit_profile.html")
