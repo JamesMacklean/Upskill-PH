4b95d7be21af69a4d9ac31f2e70e0355c800291e
Ubuntu <ubuntu@ip-172-31-13-76.ap-northeast-1.compute.internal>
2022-11-01 04:49:21 +0000

    added static root path

--- scholarium/settings.py
+++ scholarium/settings.py
@@ -125,7 +125,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static/",
 ]
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = '/static/'
 
 # temp static for admin
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
