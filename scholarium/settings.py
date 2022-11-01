b8f4f3a44b9b0530bfaec28b08d59b3d39c8ea49
James Macklean Lusuegro <jamesmacklean27@gmail.com>
2022-11-01 19:16:03 +0800

    added 127.0.0.1 in ALLOWED_HOSTS

--- scholarium/settings.py
+++ scholarium/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-k(i3^c+t7zkamnk@nut1x_inn)p_^s)sfrq(o61=i@uu6@uv&6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.249.25.89', 'scholarium.io']
+ALLOWED_HOSTS = ['54.249.25.89', 'scholarium.io', '127.0.0.1']
 
 
 # Application definition
