7b2c89de39e8928d0a969df19ff5759b765b6d3e
James Macklean Lusuegro <jamesmacklean27@gmail.com>
2022-11-07 12:50:01 +0800

    For testing runserver 003

--- scholarium/settings.py
+++ scholarium/settings.py
@@ -124,17 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
-
-#################### IMPORTANT! ####################
-############# UNCOMMENT IN PRODUCTION #############
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-####################################################
-
-#################### IMPORTANT! ####################
-############# UNCOMMENT IN DEVELOPMENT #############
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-####################################################
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
