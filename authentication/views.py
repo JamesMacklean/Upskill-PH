cfa79853e568be18841b22214270d2051e78bd8c
James Macklean Lusuegro <jamesmacklean27@gmail.com>
2022-11-08 23:14:05 +0800

    authentication views.py: modified logging 001

--- authentication/views.py
+++ authentication/views.py
@@ -51,7 +51,7 @@ def signup(request):
 
         response = requests.request("POST", url, headers=headers, data=payload, files=files)
 
-        logging.info(response)
+        logging.info(response.text)
         print(response.text)
         ############################## FOR API ##############################
         
