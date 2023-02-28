d7b3acce8e8b235a025d55f7f11e5ac162cbb1cb
James Macklean Lusuegro <jamesmacklean27@gmail.com>
2023-02-28 10:14:25 +0800

    simplified info.py code

--- scholarium/info.py
+++ scholarium/info.py
@@ -11,22 +11,23 @@ TEST_EMAIL_RECEIVER = 'jamesmacklean27@gmail.com'
 
 # API
 API_SECRET_KEY = "5af375913e149630a4dd18e2ac1548cf9bdcde9c9e03eb24e1a1f3cb5e540c4a"
+API_URL = "https://scholarium.tmtg-clone.click/v1/"
 
-API_LOGIN_ACCOUNT_URL = "https://scholarium.tmtg-clone.click/v1/login"
-API_CREATE_ACCOUNT_URL = "https://scholarium.tmtg-clone.click/v1/user/add"
-API_VERIFY_ACCOUNT_URL = "https://scholarium.tmtg-clone.click/v1/user/verify/"
+API_LOGIN_ACCOUNT_URL = API_URL+"login"
+API_CREATE_ACCOUNT_URL = API_URL+"user/add"
+API_VERIFY_ACCOUNT_URL = API_URL+"user/verify/"
 
-API_USER_URL = "https://scholarium.tmtg-clone.click/v1/me"
-API_USER_PROFILE_URL = "https://scholarium.tmtg-clone.click/v1/me/profile"
-API_USER_EMPLOYMENT_URL = "https://scholarium.tmtg-clone.click/v1/me/employment"
-API_USER_EDUCATION_URL = "https://scholarium.tmtg-clone.click/v1/me/education"
-API_USER_PROGRAMS_URL = "https://scholarium.tmtg-clone.click/v1/me/scholarship"
-API_USER_PARTNERS_URL = "https://scholarium.tmtg-clone.click/v1/me/partners"
-API_SCHOLAR_APPLY_URL = "https://scholarium.tmtg-clone.click/v1/me/scholarship"
-API_UPDATE_PASSWORD_URL = "https://scholarium.tmtg-clone.click/v1/me/password"
+API_USER_URL = API_URL+"me"
+API_USER_PROFILE_URL = API_URL+"me/profile"
+API_USER_EMPLOYMENT_URL = API_URL+"me/employment"
+API_USER_EDUCATION_URL = API_URL+"me/education"
+API_USER_PROGRAMS_URL = API_URL+"me/scholarship"
+API_USER_PARTNERS_URL = API_URL+"me/partners"
+API_SCHOLAR_APPLY_URL = API_URL+"me/scholarship"
+API_UPDATE_PASSWORD_URL = API_URL+"me/password"
 
-API_PARTNER_PROGRAMS_URL = "https://scholarium.tmtg-clone.click/v1/partner/programs/"
-API_SCHOLAR_UPDATE_URL = "https://scholarium.tmtg-clone.click/v1/partner/scholarship/"
+API_PARTNER_PROGRAMS_URL = API_URL+"partner/programs/"
+API_SCHOLAR_UPDATE_URL = API_URL+"partner/scholarship/"
 
 
 ########## ORIGINAL CODE ##########
