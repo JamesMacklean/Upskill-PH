bab24b6c545ce494206de5af6705d1bfcab20c41
JamesMacklean <jamesmacklean27@gmail.com>
2024-08-05 09:27:56 +0800

    if not authenticate_user(request):                 return redirect(f'http://accounts.upskillph.org{reverse("signin")}')

--- authentication/subdomain_middleware.py
+++ authentication/subdomain_middleware.py
@@ -1,28 +1,25 @@
-from django.http import HttpResponseForbidden
 from django.urls import reverse
 from django.shortcuts import redirect
+from django.utils.deprecation import MiddlewareMixin
+from authentication.views import authenticate_user
 
-class SubdomainMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        response = self.process_request(request)
-        if response:
-            return response
-        return self.get_response(request)
-
+class SubdomainMiddleware(MiddlewareMixin):
     def process_request(self, request):
         host = request.get_host()
         subdomain = host.split('.')[0]
 
         if subdomain == 'welcome':
             if request.path == reverse('signin'):
-                # Redirect to accounts subdomain for signin
                 return redirect(f'http://accounts.upskillph.org{reverse("signin")}')
             if request.path == reverse('signup'):
                 return redirect(f'http://accounts.upskillph.org{reverse("signup")}')
+            
+            # Check if user is authenticated for other paths on the welcome subdomain
+            if not authenticate_user(request):
+                return redirect(f'http://accounts.upskillph.org{reverse("signin")}')
+        
         elif subdomain == 'accounts':
             if request.path not in [reverse('signup'), reverse('signin')]:
                 return redirect(f'http://welcome.upskillph.org{request.path}')
+        
         return None
