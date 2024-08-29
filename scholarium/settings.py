9b6316e57bc69f80eab3ef16d7a8fe22ed5bf081
JamesMacklean <jamesmacklean27@gmail.com>
2024-08-30 07:08:19 +0800

    uncommented session cookie

--- scholarium/settings.py
+++ scholarium/settings.py
@@ -157,12 +157,12 @@ SESSION_SAVE_EVERY_REQUEST = True
 
 ########## ORIGINAL CODE ##########
 # comment these to run in DEV MODE
-# SESSION_COOKIE_DOMAIN = '.upskillph.org'
-# SESSION_COOKIE_SAMESITE = 'None'
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_DOMAIN = '.upskillph.org'
-# CSRF_COOKIE_SAMESITE = 'None'
-# CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_DOMAIN = '.upskillph.org'
+SESSION_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_DOMAIN = '.upskillph.org'
+CSRF_COOKIE_SAMESITE = 'None'
+CSRF_COOKIE_SECURE = True
 ###################################
 
 LOGGING = {
