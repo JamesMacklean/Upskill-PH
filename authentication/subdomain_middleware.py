0cb692f028a482a53254d31b14ce8afe922557ce
JamesMacklean <jamesmacklean27@gmail.com>
2024-08-30 10:07:11 +0800

    reverted original code

--- authentication/subdomain_middleware.py
+++ authentication/subdomain_middleware.py
@@ -1,85 +1,108 @@
 from django.urls import reverse
 from django.shortcuts import redirect
 from django.utils.deprecation import MiddlewareMixin
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from scholarium.info import *
 from scholarium import settings
-import time
+import jwt, time
 
 class SubdomainMiddleware(MiddlewareMixin):
-
-    # Define allowed paths for each subdomain in a dictionary
-    SUBDOMAIN_PATHS = {
-        'welcome': [
-            '',  # Home page
-            'account/',
-            'administrator/',
-            'administrator/users/',
-            'administrator/users/<int:user_id>/',
-            'administrator/partner/<int:partner_id>/',
-            'administrator/license-codes/<slug>/',
-            'certificate/',
-            'dashboard/',
-            'partner/',
-            'partner/<slug:program_slug>/application/',
-            'profile/',
-            'profile/edit/',
-            'program/<slug>/',
-            'sessions/',
-            'guidelines/',
-            'privacy/',
-            'contact/',
-            'lakip/',
-            'lakip/apply/',
-            'signout/'
-        ],
-        'accounts': [
-            'signin/',
-            'signout/',
-            'signup/',
-            'success/<user_hash>/',
-            'verify/<user_hash>/'
-        ],
-        'misamis-occidental': [
-            'lakip/',
-            'lakip/apply/'
-        ]
-    }
+    API_SECRET_KEY = API_SECRET_KEY
 
     def process_request(self, request):
         host = request.get_host()
         subdomain = host.split('.')[0]
+        
         path = request.path.rstrip('/')
-
-        allowed_paths = self.SUBDOMAIN_PATHS.get(subdomain, [])
+        accounts_redirect_paths = [
+            reverse('signup').rstrip('/'), 
+            reverse('signin').rstrip('/')
+        ]
+        accounts_redirect_prefixes = [
+            '/success/',
+            '/verify/'
+        ]
         
-        # Convert path patterns with URL parameters to simple patterns
-        path_patterns = [p.split('/')[0] for p in allowed_paths] + [p.rstrip('/').split('<')[0] for p in allowed_paths]
-        if path in path_patterns or any(path.startswith(prefix) for prefix in path_patterns):
-            return None  # Allow the request to proceed
-
-        # if subdomain in ['welcome', 'accounts', 'misamis-occidental']:
-        #     return HttpResponse(status=404)
-
-        # Handle authentication logic for other subdomains if needed
-        try:
-            user_token = request.session['user_token']
-            expires = request.session['expires']
-            current_time = int(time.time())
-            if current_time >= expires:
-                return self.signout(request, f'http://{settings.ACCOUNTS_DOMAIN}')
-        except KeyError:
-            return self.signout(request, f'http://{settings.ACCOUNTS_DOMAIN}')
+        if subdomain == 'welcome':
+            # Check if user is authenticated for other paths on the welcome subdomain
+            try:
+                # KUNG AUTHENTICATED PERO SA SIGNIN GUSTO PUMUNTA, DALHIN SA HOME
+                user_token = request.session['user_token']
+                expires = request.session['expires']
+                current_time = int(time.time())  # Get the current time in seconds since the epoch (UNIX time)
+                if current_time >= expires:
+                    # The session has expired, sign out the user
+                    return self.signout(request, f'http://{settings.ACCOUNTS_DOMAIN}')
+                
+                if path in accounts_redirect_paths or any(path.startswith(prefix) for prefix in accounts_redirect_prefixes):
+                    return redirect('home')
+            except KeyError:
+                # KUNG HINDI AUTHENTICATED PERO SA SIGNIN GUSTO PUMUNTA, DALHIN SA ACCOUNTS
+                if path in accounts_redirect_paths or any(path.startswith(prefix) for prefix in accounts_redirect_prefixes):
+                    return redirect(f'http://{settings.ACCOUNTS_DOMAIN}{path}')
+                # KUNG HINDI AUTHENTICATED AT PUMUNTA SA IBANG PAGE, ISAVE ANG URL, DALHIN SA SIGNIN
+                else:
+                    if path in accounts_redirect_paths or any(path.startswith(prefix) for prefix in accounts_redirect_prefixes):
+                        return redirect(f'http://{settings.ACCOUNTS_DOMAIN}{request.path}')
+                    else:
+                        return self.signout(request, f'http://{settings.ACCOUNTS_DOMAIN}')
+                
+        elif subdomain == 'accounts':
+            # Check if user is authenticated for other paths on the welcome subdomain
+            try:
+                # KUNG AUTHENTICATED PERO SA SIGNIN GUSTO PUMUNTA, DALHIN SA HOME
+                user_token = request.session['user_token']
+                expires = request.session['expires']
+                current_time = int(time.time())  # Get the current time in seconds since the epoch (UNIX time)
+                if current_time >= expires:
+                    # The session has expired, sign out the user
+                    return self.signout(request, f'http://{settings.ACCOUNTS_DOMAIN}')
+                
+                if path in accounts_redirect_paths or any(path.startswith(prefix) for prefix in accounts_redirect_prefixes):
+                    return redirect('home')
+                else:
+                    return redirect(f'http://{settings.DOMAIN}{request.path}')
+            except KeyError:
+                # KUNG HINDI SA SIGNIN PUPUNTA, DALHIN SA WELCOME
+                if path not in accounts_redirect_paths and not any(path.startswith(prefix) for prefix in accounts_redirect_prefixes):
+                    return redirect(f'http://{settings.DOMAIN}{request.path}')
         
+        elif subdomain == 'misamis-occidental':
+            # Allow only specific paths on the misamis-occidental subdomain
+            allowed_paths = ['/lakip/', '/privacy/']
+            if path in allowed_paths or any(path.startswith(prefix) for prefix in allowed_paths):
+                return None  # Allow the request to proceed
+            else:
+                return HttpResponse(status=404)
+        # FOR TEST CODE
+        # FOR http://127.0.0.1:8000
+        else:
+            
+            try:
+                user_token = request.session['user_token']
+                expires = request.session['expires']
+                current_time = int(time.time())  # Get the current time in seconds since the epoch (UNIX time)
+                if current_time >= expires:
+                    # The session has expired, sign out the user
+                    return self.signout(request, f'http://{host}')
+                    
+                if path in accounts_redirect_paths or any(path.startswith(prefix) for prefix in accounts_redirect_prefixes):
+                    return redirect ('home')
+            except KeyError:
+                if path not in accounts_redirect_paths and not any(path.startswith(prefix) for prefix in accounts_redirect_prefixes):
+                    return self.signout(request, f'http://{host}')
+            
         return None
     
     def signout(self, request, redirect_domain):
         """
         Signs out the user by clearing session data and printing relevant information.
         """
+        # Print cookies
         for key, value in request.COOKIES.items():
             print(f'{key}: {value}', flush=True)
 
+        # Clear sessions
         try:
             for key in list(request.session.keys()):
                 del request.session[key]
@@ -87,6 +110,7 @@ class SubdomainMiddleware(MiddlewareMixin):
         except KeyError as e:
             print(str(e), flush=True)
         
+        # Save the original URL and redirect to signin
         request.session['original_url'] = request.get_full_path()
         print(f"original_url: {request.session['original_url']}", flush=True)
         return HttpResponseRedirect(f'{redirect_domain}{reverse("signin")}')
