8ae03a105ac29a1ba3a96f2971cb2ba9fb98733b
JamesMacklean <jamesmacklean27@gmail.com>
2024-09-25 00:38:09 +0800

    final upload feature image

--- authentication/views.py
+++ authentication/views.py
@@ -742,11 +742,54 @@ def program_edit(request, partner_slug, program_slug):
         date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         program_badge = request.POST.get('program_badge')
         program_certificate = request.POST.get('program_certificate')
-        partner_logo_1 = request.POST.get('partner_logo_1') or selected_program.get('partner_logo_1')
-        partner_logo_2 = request.POST.get('partner_logo_2') or selected_program.get('partner_logo_2')
-        partner_logo_3 = request.POST.get('partner_logo_3') or selected_program.get('partner_logo_3')
-        partner_logo_4 = request.POST.get('partner_logo_4') or selected_program.get('partner_logo_4')
-        image_1 = request.POST.get('program_image_1') or selected_program.get('image_1')
+        # partner_logo_1 = request.POST.get('partner_logo_1') or selected_program.get('partner_logo_1')
+        # partner_logo_2 = request.POST.get('partner_logo_2') or selected_program.get('partner_logo_2')
+        # partner_logo_3 = request.POST.get('partner_logo_3') or selected_program.get('partner_logo_3')
+        # partner_logo_4 = request.POST.get('partner_logo_4') or selected_program.get('partner_logo_4')
+        # image_1 = request.POST.get('program_image_1') or selected_program.get('image_1')
+        
+        # Handle image uploads
+        uploaded_image_1 = request.FILES.get('program_image_1')
+        uploaded_logo_1 = request.FILES.get('partner_logo_1')
+        uploaded_logo_2 = request.FILES.get('partner_logo_2')
+        uploaded_logo_3 = request.FILES.get('partner_logo_3')
+        uploaded_logo_4 = request.FILES.get('partner_logo_4')
+        
+        # Define a function to save images
+        def save_image(uploaded_file, existing_url):
+            if uploaded_file:
+                file_name = default_storage.save(uploaded_file.name, ContentFile(uploaded_file.read()))
+                return f'https://{settings.DOMAIN}/static/images/{file_name}'
+            return existing_url
+        
+        program_image_cleared = request.POST.get('program_image_cleared') == 'true'
+        if program_image_cleared:
+            image_1 = ""  # Set to empty string if cleared
+        else:
+            image_1 = save_image(uploaded_image_1, selected_program.get('image_1'))
+        partner_logo_1_cleared = request.POST.get('partner_logo_1_cleared') == 'true'
+        if partner_logo_1_cleared:
+            partner_logo_1 = ""  # Set to empty string if cleared
+        else:
+            partner_logo_1 = save_image(uploaded_logo_1, selected_program.get('partner_logo_1'))
+        
+        partner_logo_2_cleared = request.POST.get('partner_logo_2_cleared') == 'true'
+        if partner_logo_2_cleared:
+            partner_logo_2 = ""  # Set to empty string if cleared
+        else:
+            partner_logo_2 = save_image(uploaded_logo_2, selected_program.get('partner_logo_2'))
+
+        partner_logo_3_cleared = request.POST.get('partner_logo_3_cleared') == 'true'
+        if partner_logo_3_cleared:
+            partner_logo_3 = ""  # Set to empty string if cleared
+        else:
+            partner_logo_3 = save_image(uploaded_logo_3, selected_program.get('partner_logo_3'))
+
+        partner_logo_4_cleared = request.POST.get('partner_logo_4_cleared') == 'true'
+        if partner_logo_4_cleared:
+            partner_logo_4 = ""  # Set to empty string if cleared
+        else:
+            partner_logo_4 = save_image(uploaded_logo_4, selected_program.get('partner_logo_4'))
         
         # Call the API to update the program details
         updated_program, response_message= update_program(
